13c5f90a9d397cb06c5201072314ff78674e00a2
c7a1 <bryer.c7a1@gmail.com>
2017-05-19 23:08:24 -0700

    Updated in case an empty javascript query.
    queryData was assumed to be used all the time, however upon research and testing, Javascript doesn't ALWAYS have to be used, and sometimes cannot be used. Updated function to reflect. Have not tested them but assuming they will work.

--- resources/common/db.lua
+++ resources/common/db.lua
@@ -4,30 +4,53 @@ auth = "username:password"
 bytespassword = clr.System.Text.Encoding.UTF8.GetBytes(auth);
 auth = clr.System.Convert.ToBase64String(bytespassword);
 
+local db = {}
+
 function db.GETData(identifier, callback, db, table, queryData)
+ if queryData then
+  local appJS = ["Content-type"] = "application/json", Authorization = "Basic " .. auth
+  local queryDataEncoded = json.encode(queryData)
+ else
+  local appJS = Authorization = "Basic " .. auth
+  local queryDataEncoded = nil
+ end
   PerformHttpRequest(serverUrl .. port .. "/" .. db .. "/" .. table, function(err, rText, headers)
       if rText then
         valReturn = json.decode(rText)
       end
-  end, "GET", json.encode(queryData), {["Content-type"] = 'application/json', Authorization = "Basic " .. auth})
+  end, "GET", queryDataEncoded, { appJS })
   return valReturn
 end
 
 function db.PUTData(identifier, callback, db, table, queryData)
+ if queryData then
+  local appJS = ["Content-type"] = "application/json", Authorization = "Basic " .. auth
+  local queryDataEncoded = json.encode(queryData)
+ else
+  local appJS = Authorization = "Basic " .. auth
+  local queryDataEncoded = nil
+ end
   PerformHttpRequest(serverUrl .. port .. "/" .. db .. "/" .. table, function(err, rText, headers)
       if err > 299 then
         callback(false, rText)
       else
         callback(true, 0)
       end
-  end, "PUT", json.encode(queryData), {["Content-type"] = 'application/json', Authorization = "Basic " .. auth})
+  end, "PUT", queryDataEncoded, { appJS })
 end
 
 function db.POSTData(identifier, callback, db, table, queryData)
+ if queryData then
+  local appJS = ["Content-type"] = "application/json", Authorization = "Basic " .. auth
+  local queryDataEncoded = json.encode(queryData)
+ else
+  local appJS = Authorization = "Basic " .. auth
+  local queryDataEncoded = nil
+ end
   PerformHttpRequest(serverUrl .. port .. "/" .. db .. "/" .. table, function(err, rText, headers)
       if rText then
         valReturn = json.decode(rText)
       end
-  end, "POST", json.encode(queryData), {["Content-type"] = 'application/json', Authorization = "Basic " .. auth})
+  end, "POST", queryDataEncoded, { appJS })
   return valReturn
 end
