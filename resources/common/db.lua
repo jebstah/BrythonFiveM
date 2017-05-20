84ee88e2de3ebbde0560054800b3fb6fbe36d22c
c7a1 <bryer.c7a1@gmail.com>
2017-05-19 23:56:07 -0700

    Changed db in parameters and functions to database
    Changed DB to database because db was being used as the storage name of all functions. This may have caused issues. Maybe a better name for functions and we can switch it back? However these are just parameters and their name does not entirely matter.

--- resources/common/db.lua
+++ resources/common/db.lua
@@ -6,8 +6,8 @@ auth = clr.System.Convert.ToBase64String(bytespassword);
 
 db = {}
 
-function db.GETData(identifier, callback, db, table)
-  PerformHttpRequest(serverUrl .. port .. "/" .. db .. "/" .. table, function(err, rText, headers)
+function db.GETData(identifier, callback, database, table)
+  PerformHttpRequest(serverUrl .. port .. "/" .. database .. "/" .. table, function(err, rText, headers)
       if rText then
         valReturn = json.decode(rText)
       end
@@ -15,8 +15,8 @@ function db.GETData(identifier, callback, db, table)
   return valReturn
 end
 
-function db.PUTData(identifier, callback, db, table, queryData)
-  PerformHttpRequest(serverUrl .. port .. "/" .. db .. "/" .. table, function(err, rText, headers)
+function db.PUTData(identifier, callback, database, table, queryData)
+  PerformHttpRequest(serverUrl .. port .. "/" .. database .. "/" .. table, function(err, rText, headers)
       if err > 299 then
         callback(false, rText)
       else
@@ -25,8 +25,8 @@ function db.PUTData(identifier, callback, db, table, queryData)
   end, "PUT", json.encode(queryData), {["Content-type"] = 'application/json', Authorization = "Basic " .. auth})
 end
 
-function db.POSTData(identifier, callback, db, table, queryData)
-  PerformHttpRequest(serverUrl .. port .. "/" .. db .. "/" .. table, function(err, rText, headers)
+function db.POSTData(identifier, callback, database, table, queryData)
+  PerformHttpRequest(serverUrl .. port .. "/" .. database .. "/" .. table, function(err, rText, headers)
       if rText then
         valReturn = json.decode(rText)
       end
@@ -34,8 +34,8 @@ function db.POSTData(identifier, callback, db, table, queryData)
   return valReturn
 end
 
-function db.DELETEData(identifier, callback, db, table)
-  PerformHttpRequest(serverUrl .. port .. "/" .. db .. "/" .. table, function(err, rText, headers)
+function db.DELETEData(identifier, callback, database, table)
+  PerformHttpRequest(serverUrl .. port .. "/" .. database .. "/" .. table, function(err, rText, headers)
       if err > 299 then
         callback(false, rText)
       else
