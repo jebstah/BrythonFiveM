7839fa87ff2e8da6c2cf009b0cdf26274ee01f56
c7a1 <bryer.c7a1@gmail.com>
2017-05-21 01:49:13 -0700

    Got rid of mySQL (appeared to not be used).

---
+++ resources/es_adminpanel/sv_adminpanel.lua
@@ -0,0 +1,27 @@
+-- Loading DB Class
+local db = require "../common/db.lua"
+
+-- Rcon commands
+AddEventHandler('rconCommand', function(commandName, args)
+	if commandName == 'getPlayers' then
+		TriggerEvent('es:getPlayers', function(pl)
+			for k,v in pairs(pl) do
+				RconPrint(GetPlayerName(k):gsub("%s+", "_") .. " " .. v.permission_level .. " " .. k)
+			end
+		end)
+
+		CancelEvent()
+	elseif commandName == 'ap_kick' then
+		if(GetPlayerName(args[1])) then
+			DropPlayer(args[1])
+			RconPrint('kicked')
+		end
+
+		CancelEvent()
+	elseif commandName == 'ap_slay' then
+		TriggerClientEvent('es_admin:kill', args[1])
+		RconPrint('killed')
+
+		CancelEvent()
+	end
+end)
