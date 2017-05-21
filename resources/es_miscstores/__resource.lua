41a6bbf25e86bed879a1bf8b40cf3e673dcd1ec0
c7a1 <bryer.c7a1@gmail.com>
2017-05-21 02:04:38 -0700

    Changed enhancedessentialmode from essentialmode

--- resources/es_miscstores/__resource.lua
+++ resources/es_miscstores/__resource.lua
@@ -2,7 +2,7 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 -- Requiring essentialmode
-dependency 'essentialmode'
+dependency 'enhancedessentialmode'
 
 server_script 'sv_stores.lua'
 server_script 'sv_drugs.lua'
