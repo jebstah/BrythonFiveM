d4b10366e44e3d1468896973cccbf69a61a0ef2f
c7a1 <bryer.c7a1@gmail.com>
2017-05-21 02:06:53 -0700

    Changed enhancedessentialmode from essentialmode
    lso changed fs_freeroamEnhanced from fs_freeroam

--- resources/fs_freeroamEnhanced/__resource.lua
+++ resources/fs_freeroamEnhanced/__resource.lua
@@ -1,10 +1,10 @@
-resource_type 'gametype' { name = 'fs_freeroam'}
+resource_type 'gametype' { name = 'fs_freeroamEnhanced'}
 
 -- Manifest
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 -- Requiring essentialmode
-dependency 'essentialmode'
+dependency 'enhancedessentialmode'
 
 -- General
 client_scripts {
