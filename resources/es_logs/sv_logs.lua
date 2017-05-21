a384f30a6212a8d31331b2d5e423fdd19d58a818
Holden Monroe <zaedrous@gmail.com>
2017-05-21 18:03:23 -0400

    sv_logs fixes.

--- resources/es_logs/sv_logs.lua
+++ resources/es_logs/sv_logs.lua
@@ -2,19 +2,19 @@
 
 AddEventHandler('es:chatMessage', function(source, msg, user)
 	local tstamp2 = os.date("*t", os.time())
-  local queryData = {"sender": user.identifier, 
-                    "timestamp": os.date(tstamp2.year .. "-" .. tstamp2.month .. "-" .. tstamp2.day .. " " .. tstamp2.hour .. ":" .. tstamp2.min .. ":" .. tstamp2.sec), 
-                    "message": msg }
+  local queryData = {sender = user.identifier, 
+                    timestamp = os.date(tstamp2.year .. "-" .. tstamp2.month .. "-" .. tstamp2.day .. " " .. tstamp2.hour .. ":" .. tstamp2.min .. ":" .. tstamp2.sec), 
+                    message = msg }
   
   db.GETData("",
-  function(exist, rText)
-    if exists then
-      db.PUTData(rText.uuid[1],
-        function(exist, rText)
-          if not exist then
+  function(uuid)
+    if uuid then
+      db.PUTData(uuid,
+        function(success)
+          if not success then
             print('Error adding chat message to Database!')
           end
-        end, "es_logs/chat", queryData)
+        end, "chat_logs", queryData)
     end
   end, '_uuids')
 end)
@@ -22,19 +22,19 @@ end)
 AddEventHandler('es:adminCommandRan', function(source, msg, user)
 	local tstamp2 = os.date("*t", os.time())
 	msg = table.concat(msg, " ")
-  local queryData = {"sender": user.identifier, 
-                    "timestamp": os.date(tstamp2.year .. "-" .. tstamp2.month .. "-" .. tstamp2.day .. " " .. tstamp2.hour .. ":" .. tstamp2.min .. ":" .. tstamp2.sec), 
-                    "message": msg }
+  local queryData = {sender = user.identifier, 
+                    timestamp = os.date(tstamp2.year .. "-" .. tstamp2.month .. "-" .. tstamp2.day .. " " .. tstamp2.hour .. ":" .. tstamp2.min .. ":" .. tstamp2.sec), 
+                    message = msg }
   
   db.GETData("",
-  function(exist, rText)
-    if exists then
-      db.PUTData(rText.uuid[1],
-        function(exist, rText)
-          if not exist then
+  function(uuid)
+    if uuid then
+      db.PUTData(uuid,
+        function(success)
+          if not success then
             print('Error adding admin message to Database!')
           end
-        end, "es_logs/admin", queryData)
+        end, "admin_logs", queryData)
     end
   end, '_uuids')
 end)
@@ -42,19 +42,19 @@ end)
 AddEventHandler('es:userCommandRan', function(source, msg, user)
 	local tstamp2 = os.date("*t", os.time())
 	msg = table.concat(msg, " ")
-  local queryData = {"sender": user.identifier, 
-                    "timestamp": os.date(tstamp2.year .. "-" .. tstamp2.month .. "-" .. tstamp2.day .. " " .. tstamp2.hour .. ":" .. tstamp2.min .. ":" .. tstamp2.sec), 
-                    "message": msg }
+  local queryData = {sender = user.identifier, 
+                    timestamp = os.date(tstamp2.year .. "-" .. tstamp2.month .. "-" .. tstamp2.day .. " " .. tstamp2.hour .. ":" .. tstamp2.min .. ":" .. tstamp2.sec), 
+                    message = msg }
   
   db.GETData("",
-  function(exist, rText)
-    if exists then
-      db.PUTData(rText.uuid[1],
-        function(exist, rText)
-          if not exist then
+  function(uuid)
+    if uuid then
+      db.PUTData(uuid,
+        function(success)
+          if not success then
             print('Error adding commands message to Database!')
           end
-        end, "es_logs/commands", queryData)
+        end, "command_logs", queryData)
     end
   end, '_uuids')
 end)
