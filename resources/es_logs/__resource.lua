c89b1ac4c82b2ec33cf78dfdac5d40e951b8a52c
c7a1 <bryer.c7a1@gmail.com>
2017-05-21 02:04:15 -0700

    Changed enhancedessentialmode from essentialmode

--- resources/es_logs/__resource.lua
+++ resources/es_logs/__resource.lua
@@ -2,7 +2,7 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 -- Requiring essentialmode
-dependency 'essentialmode'
+dependency 'enhancedessentialmode'
 
 server_script '../common/db.lua'
 server_script 'sv_logs.lua'
