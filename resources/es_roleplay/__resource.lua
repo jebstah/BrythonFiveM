8821c0cc383e89005e3b55642a7ffbd4509dcf24
c7a1 <bryer.c7a1@gmail.com>
2017-05-21 02:06:12 -0700

    Changed enhancedessentialmode from essentialmode
    Also changed Advanced RP to Enhanced RP

--- resources/es_roleplay/__resource.lua
+++ resources/es_roleplay/__resource.lua
@@ -1,10 +1,10 @@
-resource_type 'gametype' { name = 'Advanced RP' }
+resource_type 'gametype' { name = 'Enhanced RP' }
 
 -- Manifest
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 -- Requiring essentialmode
-dependency 'essentialmode'
+dependency 'enhancedessentialmode'
 
 -- Config
 server_script 'config/jobs.lua'
